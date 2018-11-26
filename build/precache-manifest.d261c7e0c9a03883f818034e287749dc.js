797eeb77e99c11078476110844c68118b460fc6b
Ayse Oruc <aoruc@netas.com.tr>
2018-11-26 10:21:21 +0300

    initial

---
+++ build/precache-manifest.d261c7e0c9a03883f818034e287749dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc0aeb2a5466a8c40707",
+    "url": "/static/js/main.fc0aeb2a.chunk.js"
+  },
+  {
+    "revision": "7adc55c563d3c0e545f2",
+    "url": "/static/js/1.7adc55c5.chunk.js"
+  },
+  {
+    "revision": "fc0aeb2a5466a8c40707",
+    "url": "/static/css/main.3dad57a7.chunk.css"
+  },
+  {
+    "revision": "7adc55c563d3c0e545f2",
+    "url": "/static/css/1.1ea6d810.chunk.css"
+  },
+  {
+    "revision": "ff583e9aa931252265c1937503546cf3",
+    "url": "/index.html"
+  }
+];
